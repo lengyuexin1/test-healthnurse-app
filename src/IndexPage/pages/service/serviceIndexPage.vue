ffcdec34b4a17e85e4411a70a1ed6a3f4126312a
13798392405 <928779235.com>
2025-03-01 17:47:49 +0800

    讨论区

--- src/IndexPage/pages/service/serviceIndexPage.vue
+++ src/IndexPage/pages/service/serviceIndexPage.vue
@@ -210,7 +210,7 @@ import BCNotify from '@/components/notify/index.vue'
 import WaterfallsFlow from '../platform/components/WaterfallsFlow.vue'
 import { gotoRegister, gotogoodsDetail, gotogoodsRanking } from '@/routes/goods-routes'
 import { gotoCitychange, invitationDetail } from '@/routes/user-routes'
-import { gotoServiceStore, gotoserviceDetail, toInnerPage, gotosearch, gotoShopDetail, gotoserviceLnnerPage, gotoServiceExpo, gotoLiveSelection } from '@/routes/service-routes'
+import { gotoServiceStore, gotoserviceDetail, toInnerPage,gotodiscussListPage, gotosearch, gotoShopDetail, gotoserviceLnnerPage, gotoServiceExpo, gotoLiveSelection } from '@/routes/service-routes'
 import { PlatformManage } from '@bc/sys'
 import { gotoLogin } from "@/routes/public-routes"
 import { setPageBank, bannerList, columnList, columnDetail, productList, activeDetail } from "@/api/setite-api"
